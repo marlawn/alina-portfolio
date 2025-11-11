5c8d4216fd1917a00f75c3c8d6ec97cd61ef0c4c
marlawn <marlonvergara12@gmail.com>
2025-11-10 21:54:25 -0500

    portfolio page

---
+++ src/constants/constants.tsx
@@ -0,0 +1,57 @@
+export type TPortfolioItem = {
+  title: string;
+  link: string;
+};
+
+export const portfolioItems: TPortfolioItem[] = [
+  {
+    title: "Thom Browne",
+    link: "thombrowne",
+  },
+  {
+    title: "BFA Thesis",
+    link: "bfathesis",
+  },
+  {
+    title: "Call of the Wild.",
+    link: "callofthewild",
+  },
+  {
+    title: "CFDA",
+    link: "cfda",
+  },
+  {
+    title: "Fruit of the Earth",
+    link: "fruitoftheearth",
+  },
+  {
+    title: "JANE DOE",
+    link: "janedoe",
+  },
+  {
+    title: "Journal",
+    link: "journal",
+  },
+  {
+    title: "КАЙФУЭМ",
+    link: "kayfuyem",
+  },
+  {
+    title: "News from Nowhere",
+    link: "newsfromnowhere",
+  },
+  {
+    title: "Sample Garments",
+    link: "samplegarments",
+  },
+  {
+    title: "Untitled.mp4",
+    link: "untitledmp4",
+  },
+];
+
+export const aboutMeSection = [
+  "Alina Podolyan is a New York/New Jersey based Fashion Designer currently pursuing a Bachelors in Fine Arts for Fashion Design at the Fashion Institute of Technology. ",
+  'As opposed to using certain objects as inspiration, Podolyan looks to the concepts or ideas behind these objects. She then looks to portray these concepts through the silhouette and texture of her work. Unique textiles and textile manipulation are important to her design process as Podolyan\'s goal is to create "never before done" designs that intrigue and entice consumers. Creating a powerful mood or aesthetic is her way of telling a story with her artwork and garments.',
+  "Despite specializing in Sportswear, Podolyan has a strong interest in the draping of garments and creative direction for all seasons and occasions.",
+];
