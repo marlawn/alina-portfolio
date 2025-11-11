ab0f00f2f0bfecd820192e213d6055fcfe7d678a
marlawn <marlonvergara12@gmail.com>
2025-11-10 21:53:43 -0500

    miscellaneous changes w/t css changes

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,19 +1,34 @@
-'use client'
+"use client";
 
 import { Menu } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Navigation = () => {
-    return (
-        <nav>
-            <div className="w-full hidden gap-10 sm:flex">
-                <Link href="/">Home</Link>
-                <Link href="/portfolio">Portfolio</Link>
-            </div>
+  const pathname = usePathname();
 
-            <div className="w-full flex sm:hidden justify-end">
-                <Menu size={32} />
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="relative w-full">
+      <div className="w-full hidden gap-10 sm:flex items-center justify-between">
+        <div className="flex gap-10">
+          <Link href="/" className="menu-item">
+            Home
+          </Link>
+          <Link href="/portfolio" className="menu-item">
+            Portfolio
+          </Link>
+        </div>
+      </div>
+
+      {pathname !== "/" && (
+        <h3 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          ALINA PODOLYAN
+        </h3>
+      )}
+
+      <div className="w-full flex sm:hidden justify-end">
+        <Menu size={32} />
+      </div>
+    </nav>
+  );
 };
